5a369bacae872419255b554fa4b388ea5dbe3265
steven-barritt <mail@stevenbarritt.com>
2014-09-22 20:36:46 +0100

    renamed journal to workbook
    renamed journal to workbook

--- htdocs/artefact/blog/blocktype/taggedposts/lang/en.utf8/blocktype.taggedposts.php
+++ htdocs/artefact/blog/blocktype/taggedposts/lang/en.utf8/blocktype.taggedposts.php
@@ -1,20 +1,36 @@
 <?php
 /**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2011 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package    mahara
  * @subpackage blocktype.blog/taggedposts
  * @author     Catalyst IT Ltd
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
- * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 Catalyst IT Ltd http://catalyst.net.nz
  *
  */
 
 defined('INTERNAL') || die();
 
-$string['title'] = 'Tagged journal entries';
-$string['description'] = 'Display journal entries with a particular tag (see Content -> Journal)';
-$string['blockheading'] = 'Journal entries tagged';
-$string['defaulttitledescription'] = 'If you leave this blank, the title of the journal will be used';
+$string['title'] = 'Tagged workbook entries';
+$string['description'] = 'Display workbook entries with a particular tag';
+$string['blockheading'] = 'Workbook entries tagged';
+$string['defaulttitledescription'] = 'If you leave this blank, the title of the workbook will be used';
 $string['postsperpage'] = 'Entries per page';
 $string['taglist'] = 'My tags';
 $string['notags'] = 'There are no posts tagged "%s"';
@@ -23,6 +39,6 @@ $string['postedin'] = 'in';
 $string['postedon'] = 'on';
 $string['itemstoshow'] = 'Items to show';
 $string['configerror'] = 'Error during block configuration';
-$string['showjournalitemsinfull'] = 'Show journal entries in full';
-$string['showjournalitemsinfulldesc'] = 'If checked, the journal entries will be displayed. Otherwise only the titles of the journal entries will be shown.';
+$string['showworkbookitemsinfull'] = 'Show workbook entries in full';
+$string['showworkbookitemsinfulldesc'] = 'If checked, the workbook entries will be displayed. Otherwise only the titles of the workbook entries will be shown.';
 $string['tag'] = 'Tag';
