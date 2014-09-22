71a71c4f340e2680b17174514f2ee8c2bdf7a5af
steven-barritt <mail@stevenbarritt.com>
2014-09-22 20:40:59 +0100

    change journal to workbook
    change journal to workbook

--- htdocs/artefact/blog/lang/en.utf8/artefact.blog.php
+++ htdocs/artefact/blog/lang/en.utf8/artefact.blog.php
@@ -1,21 +1,37 @@
 <?php
 /**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package    mahara
  * @subpackage lang
  * @author     Catalyst IT Ltd
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
- * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006-2009 Catalyst IT Ltd http://catalyst.net.nz
  *
  */
 
 defined('INTERNAL') || die();
 
-$string['pluginname'] = 'Journals';
+$string['pluginname'] = 'Workbooks';
 
-$string['blog'] = 'Journal';
-$string['blogs'] = 'Journals';
-$string['addblog'] = 'Create journal';
+$string['blog'] = 'Workbook';
+$string['blogs'] = 'Workbooks';
+$string['addblog'] = 'Create Workbook';
 $string['addpost'] = 'New entry';
 $string['alignment'] = 'Alignment';
 $string['allowcommentsonpost'] = 'Allow comments on your entry.';
@@ -25,45 +41,38 @@ $string['attachedfilelistloaded'] = 'Attached file list loaded';
 $string['attachedfiles'] = 'Attached files';
 $string['attachment'] = 'Attachment';
 $string['attachments'] = 'Attachments';
+$string['attachmentsimg'] = 'Images';
 $string['blogcopiedfromanotherview'] = 'Note: This block has been copied from another page. You may move it around or remove it, but you cannot change what %s is in it.';
 $string['blogdesc'] = 'Description';
 $string['blogdescdesc'] = 'e.g., ‘A record of Jill\'s experiences and reflections’.';
-$string['blogdoesnotexist'] = 'You are trying to access a journal that does not exist';
-$string['blogpostdoesnotexist'] = 'You are trying to access a journal entry that does not exist';
-$string['blogpost'] = 'Journal entry';
-$string['blogdeleted'] = 'Journal deleted';
-$string['blogpostdeleted'] = 'Journal entry deleted';
-$string['blogpostpublished'] = 'Journal entry published';
-$string['blogpostunpublished'] = 'Journal entry unpublished';
-$string['blogpostsaved'] = 'Journal entry saved';
-$string['blogsettings'] = 'Journal settings';
+$string['blogdoesnotexist'] = 'You are trying to access a workbook that does not exist';
+$string['blogpostdoesnotexist'] = 'You are trying to access a workbook entry that does not exist';
+$string['blogpost'] = 'Workbook entry';
+$string['blogdeleted'] = 'Workbook deleted';
+$string['blogpostdeleted'] = 'Workbook entry deleted';
+$string['blogpostpublished'] = 'Workbook entry published';
+$string['blogpostunpublished'] = 'Workbook entry unpublished';
+$string['blogpostsaved'] = 'Workbook entry saved';
+$string['blogsettings'] = 'Workbook settings';
 $string['blogtitle'] = 'Title';
-$string['blogtitledesc'] = 'e.g., ‘Jill’s Nursing Practicum Journal’.';
+$string['blogtitledesc'] = 'e.g., ‘Jill’s First year project Workbook’.';
 $string['border'] = 'Border';
-$string['by'] = 'by';
 $string['cancel'] = 'Cancel';
-$string['createandpublishdesc'] = 'This will create the journal entry and make it available to others.';
-$string['createasdraftdesc'] = 'This will create the journal entry, but it will not become available to others until you choose to publish it.';
-$string['createblog'] = 'Create journal';
+$string['createandpublishdesc'] = 'This will create the workbook entry and make it available to others.';
+$string['createasdraftdesc'] = 'This will create the workbook entry, but it will not become available to others until you choose to publish it.';
+$string['createblog'] = 'Create workbook';
 $string['dataimportedfrom'] = 'Data imported from %s';
-$string['defaultblogtitle'] = '%s\'s Journal';
-$string['deleteblog?'] = 'Are you sure you want to delete this journal?';
-$string['deletebloghaspost?'] = array(
-    0 => 'This journal contains 1 entry. Are you sure you want to delete this journal?',
-    1 => 'This journal contains %d entries. Are you sure you want to delete this journal?'
-);
-$string['deletebloghasview?'] = array(
-    0 => 'This journal contains entries that are used in 1 page. Are you sure you want to delete this journal?',
-    1 => 'This journal contains entries that are used in %d pages. Are you sure you want to delete this journal?'
-);
+$string['defaultblogtitle'] = '%s\'s Workbook';
+$string['delete'] = 'Delete';
+$string['deleteblog?'] = 'Are you sure you want to delete this workbook?';
 $string['deleteblogpost?'] = 'Are you sure you want to delete this entry?';
 $string['description'] = 'Description';
 $string['dimensions'] = 'Dimensions';
 $string['draft'] = 'Draft';
 $string['edit'] = 'Edit';
-$string['editblogpost'] = 'Edit journal entry';
+$string['editblogpost'] = 'Edit workbook entry';
 $string['entriesimportedfromleapexport'] = 'Entries imported from a LEAP export that were not able to be imported elsewhere';
-$string['errorsavingattachments'] = 'An error occurred while saving journal entry attachments';
+$string['errorsavingattachments'] = 'An error occurred while saving workbook entry attachments';
 $string['horizontalspace'] = 'Horizontal space';
 $string['insert'] = 'Insert';
 $string['insertimage'] = 'Insert image';
@@ -72,17 +81,20 @@ $string['mustspecifytitle'] = 'You must specify a title for your entry';
 $string['mustspecifycontent'] = 'You must specify some content for your entry';
 $string['name'] = 'Name';
 $string['newattachmentsexceedquota'] = 'The total size of the new files that you have uploaded to this entry would exceed your quota. You may be able to save the entry if you remove some of the attachments you have just added.';
-$string['newblog'] = 'New journal';
-$string['newblogpost'] = 'New journal entry in journal "%s"';
+$string['newblog'] = 'New workbook';
+$string['newblogpost'] = 'New workbook entry in workbook "%s"';
+$string['newblankblogpost'] = 'New workbook entry';
 $string['newerposts'] = 'Newer entries';
-$string['nodefaultblogfound'] = 'No default journal found. This is a bug in the system. To fix it, you need to enable the multiple journals option on the <a href="%saccount/index.php">account settings</a> page.';
+$string['nodefaultblogfound'] = 'No default workbook found. This is a bug in the system. To fix it, you need to enable the multiple workbooks option on the <a href="%saccount/index.php">account settings</a> page.';
 $string['nopostsyet'] = 'No entries yet.';
 $string['noimageshavebeenattachedtothispost'] = 'No images have been attached to this entry. You need to upload or attach an image to the entry before you can insert it.';
 $string['nofilesattachedtothispost'] = 'No attached files';
-$string['noresults'] = 'No journal entries found';
+$string['noresults'] = 'No workbook entries found';
 $string['olderposts'] = 'Older entries';
 $string['post'] = 'entry';
 $string['postbody'] = 'Entry';
+$string['postbodyimg'] = 'Comments';
+$string['posturl'] = 'URL';
 $string['postbodydesc'] = ' ';
 $string['postedon'] = 'Posted on';
 $string['postedbyon'] = 'Posted by %s on %s';
@@ -110,10 +122,10 @@ $string['thisisdraftdesc'] = 'When your entry is a draft, no one except you can 
 $string['title'] = 'Title';
 $string['update'] = 'Update';
 $string['verticalspace'] = 'Vertical space';
-$string['viewblog'] = 'View journal';
-$string['youarenottheownerofthisblog'] = 'You are not the owner of this journal.';
-$string['youarenottheownerofthisblogpost'] = 'You are not the owner of this journal entry.';
-$string['cannotdeleteblogpost'] = 'An error occurred removing this journal entry.';
+$string['viewblog'] = 'View workbook';
+$string['youarenottheownerofthisblog'] = 'You are not the owner of this workbook.';
+$string['youarenottheownerofthisblogpost'] = 'You are not the owner of this workbook entry.';
+$string['cannotdeleteblogpost'] = 'An error occurred removing this workbook entry.';
 
 $string['baseline'] = 'Baseline';
 $string['top'] = 'Top';
@@ -134,27 +146,16 @@ $string['copynocopy'] = 'Skip this block entirely when copying the page';
 $string['viewposts'] = 'Copied entries (%s)';
 $string['postscopiedfromview'] = 'Entries copied from %s';
 
-$string['youhavenoblogs'] = 'You have no journals.';
-$string['youhaveoneblog'] = 'You have 1 journal.';
-$string['youhaveblogs'] = 'You have %s journals.';
+$string['youhavenoblogs'] = 'You have no workbooks.';
+$string['youhaveoneblog'] = 'You have 1 workbook.';
+$string['youhaveblogs'] = 'You have %s workbooks.';
 
 $string['feedsnotavailable'] = 'Feeds are not available for this artefact type.';
 $string['feedrights'] = 'Copyright %s.';
 
-$string['enablemultipleblogstext'] = 'You have one journal. If you would like to start a second one, enable the multiple journals option on the <a href="%saccount/index.php">account settings</a> page.';
-$string['hiddenblogsnotification'] = 'Additional journal(s) have been made for you, but your account does not have the multiple journals option activated. You can enable it on the <a href="%saccount/index.php">account settings</a> page.';
+$string['enablemultipleblogstext'] = 'You have one workbook. If you would like to start a second one, enable the multiple workbooks option on the <a href="%saccount/index.php">account settings</a> page.';
+$string['hiddenblogsnotification'] = 'Additional workbook(s) have been made for you, but your account does not have the multiple workbooks option activated. You can enable it on the <a href="%saccount/index.php">account settings</a> page.';
 
 $string['shortcutaddpost'] = 'Add a new entry to';
 $string['shortcutgo'] = 'Go';
 $string['shortcutnewentry'] = 'New entry';
-
-$string['duplicatedblog'] = 'Duplicated journal';
-$string['existingblogs'] = 'Existing journals';
-$string['duplicatedpost'] = 'Duplicated journal entry';
-$string['existingposts'] = 'Existing journal entries';
-
-$string['progress_blog'] = 'Add a journal';
-$string['progress_blogpost'] = array(
-    'Add 1 entry to a journal',
-    'Add %s entries to a journal',
-);
